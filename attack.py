c6139b99f3c9c4c988196040d04b359d5794eb77
mad-weirdie <mwerries@uoregon.edu>
2022-02-03 19:59:14 -0800

    created attack.py file

---
+++ attack.py
@@ -0,0 +1,20 @@
+"""
+	Title: CIS 433 Audio
+"""
+# Cropping - small and large
+
+# Pitch Shift
+
+# Volume change
+
+# Compression - mp3?
+
+# Faking a signature that's not supposed to exist
+
+# Changing the number of samples per second
+
+# Adding noise
+
+# Low-pass/High-pass filter
+
+# Time-scale modification
